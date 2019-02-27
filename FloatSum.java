0afd5b1be4359388a71a77b68d364b68cd3a7dc2
Rahat Masum <rahatapu103@gmail.com>
2019-02-27 17:49:01 -0600

    Sums two floating point

---
+++ FloatSum.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrankcode;
+
+/**
+ *
+ * @author APU
+ */
+public class FloatSum {
+    public Float floatSum(Float A, Float B)
+    {
+        Float sumFc = A+B;
+        return sumFc;
+    }
+}
