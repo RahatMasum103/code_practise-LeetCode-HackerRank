d16633599fd8baf74775b4556be3324f7b9ce849
Rahat Masum <rahatapu103@gmail.com>
2019-09-13 22:59:05 -0700

    Accepted LCP

--- lcs_prefix.java
+++ lcs_prefix.java
@@ -18,20 +18,39 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         int size = strs.length;
 
+        if(size == 0) return "";
+        
+        String lcp = strs[0];
+        System.out.println("debug0... "+lcp);
+        for(int i=1; i<size; i++)
+        {
+            //System.out.println("debug... "+strs[i].indexOf(lcp));
+            //System.out.println("debug before ... "+lcp);
+            while(strs[i].indexOf(lcp) !=0)
+            {
+                lcp = lcp.substring(0,lcp.length()-1); //inclusive, exclusive
+                System.out.println("debug i... "+lcp);
+                if(lcp.isEmpty()) return "";
+            }
+        }
+        
+        return lcp;
+        
+        /*
 		Arrays.sort(strs); 		// sort arays as longest match
 		
 
-		/* find the minimum length from first and last string */
+		 find the minimum length from first and last string 
 		int difference = Math.min(strs[0].length(), strs[size-1].length()); 
 
-		/* find the common prefix between the first and 
-	           last string */
+		 find the common prefix between the first and last string 
 		int i = 0; 
 		while (i < difference && strs[0].charAt(i) == strs[size-1].charAt(i)) 
 			i++; 
 
 		String lcs = strs[0].substring(0, i); 
 		//System.out.println(lcs);
-        return lcs;
+        */
+        //return lcs;
     }
 }
