d4b35506fe29fbd89c08276006953637ad85bad9
Rahat Masum <rahatapu103@gmail.com>
2019-09-15 04:21:10 -0700

    Merger to create one list

---
+++ merger_sorted_list.java
@@ -0,0 +1,52 @@
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+
+/**************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        ListNode temp = new ListNode(0);
+        ListNode merge = temp;
+        
+        
+        
+        while(l1 != null && l2!=null)
+        {
+            if(l1.val < l2.val)
+            {
+                merge.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                merge.next = l2;
+                l2 = l2.next;
+            }
+            merge = merge.next;
+            //System.out.println("debug.. "+temp.next);
+        }
+        if(l1 !=null)
+        {
+            merge.next = l1;
+        }
+        
+        if(l2 !=null)
+        {
+            merge.next = l2;
+        }
+        
+        return temp.next;
+    }
+}
