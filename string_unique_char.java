ab3494c5f04bddc5aefa7ad21d18916f79707d1f
Rahat Masum <rahatapu103@gmail.com>
2019-10-01 17:10:33 -0700

    Index of first unique character

---
+++ string_unique_char.java
@@ -0,0 +1,44 @@
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+/*****************************************************************/
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int len = s.length();
+        HashMap<Character,Integer> hmap = new HashMap<>();
+        
+        for(int i=0; i<len; i++)
+        {
+            char c = s.charAt(i);
+            if(hmap.containsKey(c))
+            {
+                hmap.put(c,-1);
+            }
+            else{
+                hmap.put(c,i);
+            }
+        }
+        
+        int idx = Integer.MAX_VALUE;
+        for(char c: hmap.keySet())
+        {
+            if(hmap.get(c)>-1)
+            {
+                idx=Math.min(idx,hmap.get(c));
+            }
+        }
+        
+        return idx;
+        
+        
+    }
+}
