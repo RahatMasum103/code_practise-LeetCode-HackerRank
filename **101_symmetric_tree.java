f3bc7301199c65ab36d2cca0da1314346b63b495
Rahat Masum <rahatapu103@gmail.com>
2019-10-08 22:32:50 -0700

    Tree is mirror or symmetric at center

---
+++ **101_symmetric_tree.java
@@ -0,0 +1,51 @@
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) 
+            return true;
+        
+        return isMirror(root,root);
+    }
+    
+    public boolean isMirror(TreeNode t1, TreeNode t2)
+    {
+        if(t1 == null && t2 == null) 
+            return true;
+        
+        if(t1 !=null && t2!= null && t1.val == t2.val)
+        {
+            return isMirror(t1.left,t2.right) && isMirror(t1.right,t2.left);
+        }
+     
+        return false;        
+       
+    }
+}
