aef744fda8d89463f59befb679276df5f24f2e08
Rahat Masum <rahatapu103@gmail.com>
2019-10-22 01:57:51 -0700

    Sum of Left Leaves of Binary Tree

---
+++ *404_leaf_sum.java
@@ -0,0 +1,45 @@
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+               
+        int sum = findSum(root,false);
+        return sum;
+    }
+    
+    public int findSum(TreeNode root, boolean left)
+    {
+        if(root == null) 
+            return 0;
+        
+        int recurSum = 0;
+        
+        if(root.left == null && root.right == null && left)
+        {
+            return root.val;
+        }
+        
+        recurSum += findSum(root.left, true) + findSum(root.right, false);
+        
+        return recurSum;
+    }
+}
