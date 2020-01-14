816f2a29cfea18b22427dc260cea032410a2c75c
Rahat Masum <rahatapu103@gmail.com>
2020-01-13 23:55:02 -0800

    print # with stair pattern

---
+++ hckr_stair_case.java
@@ -0,0 +1,72 @@
+/*
+Function Description
+
+Complete the staircase function in the editor below. It should print a staircase as described above.
+
+staircase has the following parameter(s):
+
+n: an integer
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Constraints
+
+ .
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+
+        for(int i=0 ; i<n ;i++){
+            for(int j = 0; j <= n-i-2; j++){
+                System.out.print(" ");
+            }
+            for(int j = n-i-1 ; j< n; j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
