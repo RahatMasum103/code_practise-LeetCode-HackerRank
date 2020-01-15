c78969d749c9f183cb86fd2c495e7a018bb5c7f8
Rahat Masum <rahatapu103@gmail.com>
2020-01-14 17:03:56 -0800

    12 hr to 24 date format

---
+++ kr_parse_date.java
@@ -0,0 +1,81 @@
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+s: a string representing time in  hour format
+Input Format
+
+A single string  containing a time in -hour clock format (i.e.:  or ), where  and .
+
+Constraints
+
+All input times are valid
+Output Format
+
+Convert and print the given time in -hour format, where .
+
+Sample Input 0
+
+07:05:45PM
+Sample Output 0
+
+19:05:45
+
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static String timeConversion(String s) {
+        /*
+         * Write your code here.
+         */
+        DateFormat inputStrFormat = new SimpleDateFormat( "hh:mm:ssaa");
+        DateFormat outputStrFormat = new SimpleDateFormat( "HH:mm:ss");
+        Date date = null;
+
+        try{
+            
+        date = inputStrFormat.parse(s);
+
+        }
+        catch ( ParseException e )
+        {
+                e.printStackTrace();
+        }
+
+
+        String result= null;
+
+        if(date != null)
+        {
+            result = outputStrFormat.format(date);
+        }
+        return result;
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scan.nextLine();
+
+        String result = timeConversion(s);
+
+        bw.write(result);
+        bw.newLine();
+
+        bw.close();
+    }
+}
