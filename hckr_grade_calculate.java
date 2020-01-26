f3d9a738e6af0ccd9b2b0fa092dc59a1ade1b177
Rahat Masum <rahatapu103@gmail.com>
2020-01-25 20:42:57 -0800

    Automate grade with conditions and round up

---
+++ hckr_grade_calculate.java
@@ -0,0 +1,102 @@
+Function Description
+
+Complete the function gradingStudents in the editor below. It should return an integer array consisting of rounded grades.
+
+gradingStudents has the following parameter(s):
+
+grades: an array of integers representing grades before rounding
+Input Format
+
+The first line contains a single integer, , the number of students.
+Each line  of the  subsequent lines contains a single integer, , denoting student 's grade.
+
+Constraints
+
+Output Format
+
+For each , print the rounded grade on a new line.
+
+Sample Input 0
+
+4
+73
+67
+38
+33
+Sample Output 0
+
+75
+67
+40
+33
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+    int size = grades.size();
+    for(int i=0; i< size; i++)
+    {
+        int grd = grades.get(i);
+        if(grd >= 38 && (grd % 5 >=3))
+        {
+            grades.set(i, (grd + (5-grd %5))); 
+        }
+    }  
+    return grades;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
