51c81d36cf0427dd2d8ffa01a95b0ccef9c94abf
Rahat Masum <rahatapu103@gmail.com>
2020-01-25 20:54:53 -0800

    Find numebr of fruits fall in the house

---
+++ hckr_fruit_fall_position.java
@@ -0,0 +1,124 @@
+
+Function Description
+
+Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+countApplesAndOranges has the following parameter(s):
+
+s: integer, starting point of Sam's house location.
+t: integer, ending location of Sam's house location.
+a: integer, location of the Apple tree.
+b: integer, location of the Orange tree.
+apples: integer array, distances at which each apple falls from the tree.
+oranges: integer array, distances at which each orange falls from the tree.
+Input Format
+
+The first line contains two space-separated integers denoting the respective values of  and .
+The second line contains two space-separated integers denoting the respective values of  and .
+The third line contains two space-separated integers denoting the respective values of  and .
+The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
+The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
+
+Constraints
+
+Output Format
+
+Print two integers on two different lines:
+
+The first integer: the number of apples that fall on Sam's house.
+The second integer: the number of oranges that fall on Sam's house.
+Sample Input 0
+
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+Sample Output 0
+
+1
+1
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+
+        int countApple =0; 
+        int countOrange = 0;
+
+        for(int i=0; i<apples.length; i++)
+        {
+            if(a+apples[i] >= s && a+apples[i] <= t)
+            {
+                countApple++;
+            }
+        } 
+
+        for(int i=0; i<oranges.length; i++)
+        {
+            if(b+oranges[i] >= s && b+oranges[i] <= t)
+            {
+                countOrange++;
+            }
+        } 
+
+        System.out.println(countApple);
+        System.out.println(countOrange);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
+    }
+}
