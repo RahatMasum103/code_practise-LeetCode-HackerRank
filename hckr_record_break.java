b575ee623f7de23efc24451dca49138e8439003a
Rahat Masum <rahatapu103@gmail.com>
2020-01-29 01:07:46 -0800

    Find best and worst record break for a game season

---
+++ hckr_record_break.java
@@ -0,0 +1,117 @@
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+
+For example, assume her scores for the season are represented in the array . Scores are in the same order as the games played. She would tabulate her results as follows:
+
+                                 Count
+Game  Score  Minimum  Maximum   Min Max
+ 0      12     12       12       0   0
+ 1      24     12       24       0   1
+ 2      10     10       24       1   1
+ 3      24     10       24       1   1
+Given Maria's scores for a season, find and print the number of times she breaks her records for most and least points scored during the season.
+
+Function Description
+
+Complete the breakingRecords function in the editor below. It must return an integer array containing the numbers of times she broke her records. Index  is for breaking most points records, and index  is for breaking least points records.
+
+breakingRecords has the following parameter(s):
+
+scores: an array of integers
+Input Format
+
+The first line contains an integer , the number of games.
+The second line contains  space-separated integers describing the respective values of .
+
+Constraints
+
+Output Format
+
+Print two space-seperated integers describing the respective numbers of times her best (highest) score increased and her worst (lowest) score decreased.
+
+Sample Input 0
+
+9
+10 5 20 20 4 5 2 25 1
+Sample Output 0
+
+2 4
+Explanation 0
+
+The diagram below depicts the number of times Maria broke her best and worst records throughout the season:
+
+image
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+
+        int min, max;
+        min = max = scores[0];
+        int bestCount, worstCount;
+        bestCount = worstCount = 0;
+        int record[] = new int[2];
+
+        for(int i=1; i<scores.length; i++)
+        {
+            if(max < scores[i]){
+                max = scores[i];
+                bestCount++;
+            }
+            if(min > scores[i]){
+                min = scores[i];
+                worstCount++;
+            }
+        }
+        record[0] = bestCount;
+        record[1] = worstCount;
+
+        return record;
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
