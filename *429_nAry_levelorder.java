fea2eed5db0d7e1f6073dc22c913e0f05eb5355f
Rahat Masum <rahatapu103@gmail.com>
2020-02-09 17:06:43 -0800

    N Ary Tree Level Order Traversal

---
+++ *429_nAry_levelorder.java
@@ -0,0 +1,64 @@
+Given an n-ary tree, return the level order traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+ 
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [[1],[3,2,4],[5,6]]
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+               
+        List<List<Integer>> result = new ArrayList();
+        if(root == null) // edge case: empty tree
+            return result; 
+        
+        Queue<Node> q = new LinkedList();
+        
+        q.offer(root); // insert first Node, FIFO
+        
+        while(!q.isEmpty())
+        {
+            List<Integer> list = new ArrayList(); // level based list
+            Queue<Node> tempQ = new LinkedList(); //temporary queue
+            
+            for(Node n : q) // each Node in the Queue
+            {
+                list.add(n.val); // level root value of Node
+                for(Node child : n.children) // ant existing children need to traverse
+                {
+                    tempQ.offer(child); // add into the temporary Queue
+                }
+            }
+            
+            result.add(list); // whole level done
+            q = tempQ; // replace the Queu (same as poll() the top and then offer())
+        }
+        return result;
+    }
+}
