0fc4b26920125657351ddef0cc3f0ef102a73b11
Rahat Masum <rahatapu103@gmail.com>
2020-02-16 16:19:00 -0800

    With no extra space

--- **73_set_zero_matrix.java
+++ **73_set_zero_matrix.java
@@ -27,6 +27,76 @@ Output:
   [0,3,1,0]
 ]
 
+/* O(1) Space */
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        boolean isFirstRow = false;
+        boolean isFirstCol = false;
+        
+        for(int i=0; i<row; i++)
+        {
+            if(matrix[i][0] == 0)
+            {
+                isFirstCol = true;
+            }
+        }
+        
+        for(int j=0; j<col; j++)
+        {
+            if(matrix[0][j] == 0)
+            {
+                isFirstRow = true;
+            }
+        }
+        
+        for(int i=1; i<row; i++)
+        {
+            for(int j=1; j<col; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+         for(int i=1; i<row; i++)
+        {
+            for(int j=1; j<col; j++)
+            {
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                {
+                    matrix[i][j] =0;
+                }
+            }
+         }
+        
+        if(isFirstRow == true)
+        {
+            for(int j=0; j<col; j++)
+            {
+                matrix[0][j] =0;
+            }
+        }
+        
+        if(isFirstCol == true)
+        {
+            for(int i=0; i<row; i++)
+            {
+                matrix[i][0] =0;
+            }
+        }
+        
+        return;
+                
+    }
+}
+
+
 /*    O(M x N) solution */
 
 class Solution {
